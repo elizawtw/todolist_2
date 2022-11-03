606c128f6fbf146c93f5a729c6449b250d4f7333
eliza <twew02@gmail.com>
2022-11-03 19:27:04 -0400

    update ports

--- app.js
+++ app.js
@@ -138,6 +138,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 5502, function () {
-  console.log("Server started on port 5502");
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port = 5502;
+}
+
+
+app.listen(port, function () {
+  console.log("Server started on port " + port);
 });
